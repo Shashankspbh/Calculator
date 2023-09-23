f595a0d31ed11be0ab856db0a11f17e3481aeedf
Shashank Singh <Shashank Singh>
2023-09-23 23:50:47 +0530

    Added the base logic of calculator

--- script.js
+++ script.js
@@ -10,7 +10,11 @@ const divide = (x, y) => x / y;
 // Initializing Variables and operators
 let firstNum = 0;
 let secondNum = 0;
-let operator;
+let operator; //+
+let displayValue = ""; //5
+let numberClicks = 0;
+let firstRun = 1;
+let afterEquals = 0;
 
 //Operate function which will take firstNum, secondNum and thrirdNum as input and call the add, subtract, multiple and divide functions
 
@@ -22,3 +26,59 @@ const operate = function (x, y, operator) {
   else if (operator == "/") result = divide(x, y);
   return result;
 };
+
+//Initialize the DOM
+const numButtons = [...document.querySelectorAll(".num-button")];
+const operatorButtons = [...document.querySelectorAll(".operator-button")];
+const display = document.querySelector(".display");
+const equalButton = document.querySelector('button[operator-key="="]');
+const acButton = document.querySelector('button[operator-key="AC"]');
+
+// console.log(operatorButtons);
+//
+function numClick(e) {
+  buttonValue = e.target.textContent;
+  displayValue = displayValue + buttonValue;
+  display.textContent = displayValue;
+}
+function operatorClick(e) {
+  if (firstRun) {
+    operator = e.target.getAttribute("operator-key");
+    firstNum = Number(displayValue);
+    // console.log("FirstNumber Updated to: ", firstNum);
+    displayValue = "";
+    firstRun -= 1;
+  } else {
+    operator = e.target.getAttribute("operator-key");
+    secondNum = Number(displayValue);
+    // console.log(firstNum, secondNum, operator);
+    displayValue = operate(firstNum, secondNum, operator);
+    display.textContent = displayValue;
+    firstNum = displayValue;
+    displayValue = "";
+  }
+}
+/*Event Listener fro Number Buttons
+ */
+numButtons.forEach((button) => button.addEventListener("click", numClick));
+operatorButtons.forEach((button) =>
+  button.addEventListener("click", operatorClick)
+);
+/*Event Listener for Equal Button
+ */
+equalButton.addEventListener("click", function () {
+  secondNum = Number(displayValue);
+  //   console.log(firstNum, secondNum, operator);
+  displayValue = operate(firstNum, secondNum, operator);
+  display.textContent = displayValue;
+  firstRun = 1;
+});
+/* Event Listener for AC Button
+ */
+acButton.addEventListener("click", function () {
+  firstNum = 0;
+  secondNum = 0;
+  displayValue = "";
+  display.textContent = "";
+  firstRun = 1;
+});
